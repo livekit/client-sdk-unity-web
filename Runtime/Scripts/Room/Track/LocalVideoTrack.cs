7c1871bb8964de5acf87a655142a72ee5616e905
Mohammed AlMadhoun <mohelm97@gmail.com>
2023-09-03 14:20:56 +0300

    Fix code style for SetProcessor

--- Runtime/Scripts/Room/Track/LocalVideoTrack.cs
+++ Runtime/Scripts/Room/Track/LocalVideoTrack.cs
@@ -42,10 +42,11 @@ namespace LiveKit
             return Acquire<JSPromise>(JSNative.CallMethod(NativeHandle, "restartTrack"));
         }
 
-        public JSPromise SetProcessor (JSHandle processor, bool showProcessedStreamLocally = true) {
-            JSNative.PushObject (processor);
-            JSNative.PushBoolean (showProcessedStreamLocally);
-            return Acquire<JSPromise> (JSNative.CallMethod (NativeHandle, "setProcessor"));
+        public JSPromise SetProcessor(JSHandle processor, bool showProcessedStreamLocally = true)
+        {
+            JSNative.PushObject(processor);
+            JSNative.PushBoolean(showProcessedStreamLocally);
+            return Acquire<JSPromise>(JSNative.CallMethod (NativeHandle, "setProcessor"));
         }
     }
 }
